81a90fd44c7fd44acca26ee0ae0bf8ef0abf0387
shaneashford <psashford@gmail.com>
2022-01-15 17:49:33 -0500

    Typed f-string rounding program.

---
+++ Input, Processing, and Output (Module 2)/2-21 f_string_rounding.py
@@ -0,0 +1,11 @@
+# This program demonstrates how a floating-point
+# number is displayed with no formatting.
+amount_due = 5000.0
+monthly_payment = amount_due / 12.0
+print(f'The monthly payment is {monthly_payment}.')
+
+# This program demonstrates how a floating-point
+# number can be rounded.
+amount_due = 5000.0
+monthly_payment = amount_due / 12.0
+print(f'The monthly payment is {monthly_payment:.2f}.')
