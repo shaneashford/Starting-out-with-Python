dedb7b68cf9ca21356002c93ed716d80fa1af70f
shaneashford <psashford@gmail.com>
2022-01-16 11:53:10 -0500

    Added program requirements as comments.

---
+++ Input, Processing, and Output (Module 2)/assignments/module 2/ashford2/program2_2.py
@@ -0,0 +1,5 @@
+# Write a program that prompts the user to enter both the numerator and denominator of an improper fraction. Review the Python math operators on page 54 and use an f_string to express the improper fraction as a mixed number.
+# Example Output
+# Enter the numerator 34
+# Enter the denominator 7
+# As a mixed number that is 4 and 6/7
---
+++ Input, Processing, and Output (Module 2)/assignments/module 2/ashford2/program2_3.py
@@ -0,0 +1,7 @@
+# A taxable item is available in multiple quantities. Write a program that prompts the user for the unit price of the item and the number being purchased. The sales tax rate is 7%. Store that to properly named constant  as a decimal. Calculate and print the subtotal, the sales tax, and the total amount due. Display commas for numbers of 1,000 or more. Note the apostrophe in item's in the Example Output below. Your prompt should do that, too.
+# Example Output
+# Enter the item's unit price 749.99
+# Enter the quantity 2
+# The subtotal is $1,499.98
+# Sales tax is $105.00
+# Total due is $1,604.98 
