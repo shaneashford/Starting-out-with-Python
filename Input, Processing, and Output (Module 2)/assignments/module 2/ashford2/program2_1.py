005cba70a144e31e2edf1e08c0740c2a0473fa69
shaneashford <psashford@gmail.com>
2022-01-16 14:11:30 -0500

    Reformatted text.

--- Input, Processing, and Output (Module 2)/assignments/module 2/ashford2/program2_1.py
+++ Input, Processing, and Output (Module 2)/assignments/module 2/ashford2/program2_1.py
@@ -1,11 +1,7 @@
 # Phillip Shane Ashford 2510522
 
 # ASSIGNMENT
-# Write a program that converts miles to kilometers. Look up the conversion
-#   factor and assign it to a properly named constant (see page 80). The
-#   program should permit the user to enter the miles perhaps with decimals,
-#   from the keyboard. Print the result showing both distances using an
-#   f_string with the kilometers accurate to three decimal places.
+# Write a program that converts miles to kilometers. Look up the conversion factor and assign it to a properly named constant (see page 80). The program should permit the user to enter the miles perhaps with decimals, from the keyboard. Print the result showing both distances using an f_string with the kilometers accurate to three decimal places.
 # Required Output
 # Enter the miles 12.75
 # 12.75 miles is 20.519 kilometers
