423a96cc9d8bc86db3ff39405a7863a53c0fe117
shaneashford <psashford@gmail.com>
2022-01-22 16:01:34 -0500

    Added .pyy extension to 2 programs and initiated them with comments.

---
+++ Decision Structures and Boolean Logic (Module 3)/Assignments/ashford3/program3_2.py
@@ -0,0 +1,6 @@
+# Phillip Shane Ashford 2510522
+
+# Write a program that prompts the user to enter an odd multiple of 19 that is more than 60 and less than 200. Respond to good inputs with "Good input" and displaying the other factor. Respond to incorrect inputs with "Bad input". Use logical operators and just one if expression (no nested ifs).
+########
+
+# Pseudocode
---
+++ Decision Structures and Boolean Logic (Module 3)/Assignments/ashford3/program3_3.py
@@ -0,0 +1,6 @@
+# Phillip Shane Ashford 2510522
+
+# Write a program that prompts the user to enter a two-digit integer. If the user's input is acceptable, prompt the user to enter another, DIFFERENT two-digit integer. If this input is also acceptable, the program should determine which integer is larger, and by how much, and print the result (see example outputs). If either input is unacceptable, the program should respond with a statement identifying the bad input. Use nested decision blocks and pay close attention to proper indentation.
+########
+
+# Pseudocode
