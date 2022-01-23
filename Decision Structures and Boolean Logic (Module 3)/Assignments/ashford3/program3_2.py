7a6fb5c9aebcbfc4af906dd7911b3ed3973088ed
shaneashford <psashford@gmail.com>
2022-01-23 16:19:53 -0500

    Wrote a simple arithmetic program.

--- Decision Structures and Boolean Logic (Module 3)/Assignments/ashford3/program3_2.py
+++ Decision Structures and Boolean Logic (Module 3)/Assignments/ashford3/program3_2.py
@@ -4,3 +4,17 @@
 ########
 
 # Pseudocode
+# Prompt for input from user
+# Calculate if input meets requirements (if the input / 19 contains no remainder and is > 60 and < 200 it does)
+# Print response to input
+
+# Prompt for input from user
+num_to_test = int(input('Enter an odd multiple of 19 that is more than 60 and less than 200 '))
+
+# Calculate if input meets requirements (if the input % 2 equals 1 AND the input % 19 contains equals 0 AND the input is > 60 and < 200 it meets the requirements)
+if num_to_test % 2 == 1 and num_to_test % 19 == 0 and num_to_test > 60 and num_to_test < 200:
+# Print response to user's input
+    print(f'Good input. The other factor is {int(num_to_test / 19)}.')
+else:
+# Print response to user's input
+    print('Bad input')
