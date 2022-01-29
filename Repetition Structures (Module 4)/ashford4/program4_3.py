31d2a3ea594ec9f885788c537ce0298ed8e5faac
shaneashford <psashford@gmail.com>
2022-01-29 17:31:43 -0500

    Wrote nested loop program.

--- Repetition Structures (Module 4)/ashford4/program4_3.py
+++ Repetition Structures (Module 4)/ashford4/program4_3.py
@@ -1,8 +1,36 @@
 # Phillip Shane Ashford 2510522
 
-# Write a program that uses nested loops to print the output exactly as shown below. NOTE: You must use nested loops.
-# Required Output
-# prob4_3
-########
-
+#################################
 # Pseudocode
+
+# Initialize variable to hold range value
+# Initialize counter to control and exit while loop when complete
+# Write while loop that tracks the value of the counter
+    # Write for loop with range function assigned to variable
+        # Print output
+    # Print to new line after each iteration of the for loop
+    # Reduce the value in the range variable by 1 (range -= 1) for the next loop iteration
+    # Reduce the counter by 1 for the next loop iteration
+
+#################################
+
+# Initialize variable to hold range value
+range_value = 10
+
+# Initialize counter to control and exit while loop when complete
+counter = 9
+
+# Write while loop that tracks the value of the counter
+while counter < 10 and counter > -1:
+    # Write for loop with range function assigned to variable
+    for num in range(range_value):
+        # Print output
+        print(num, end='')
+    # Print to new line for desired output
+    print()
+
+    # Reduce the value in the range variable by 1 for the next loop iteration
+    range_value -= 1
+
+    # Reduce the counter by 1 for the next loop iteration
+    counter -= 1
